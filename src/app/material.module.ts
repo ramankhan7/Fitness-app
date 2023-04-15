d1ce47eba6ed4ac734c299a50bea56bda450a545
raman <khanahmadi.raman@yahoo.com>
2023-04-15 12:29:11 +0430

    add material input

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
-    imports: [MatButtonModule,MatIconModule],
-    exports: [MatButtonModule,MatIconModule]
+    imports: [MatFormFieldModule,MatInputModule],
+    exports: [MatFormFieldModule,MatInputModule]
 
 })
 export class MaterialModule {
