7f63eb10739b75b5c82ee9bc0bb16628acff7a4c
raman <khanahmadi.raman@yahoo.com>
2023-04-15 10:40:57 +0430

    confige app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
+import { NewTrainingComponent } from './new-training/new-training.component';
+import { CurrentTrainingComponent } from './current-training/current-training.component';
+import { PastTrainingComponent } from './past-training/past-training.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    TrainingComponent,
+    NewTrainingComponent,
+    CurrentTrainingComponent,
+    PastTrainingComponent
   ],
   imports: [
     BrowserModule,
