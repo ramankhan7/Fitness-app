750c9e2b2555172e43c220bf5f9eda1a6d2f5eb4
raman <khanahmadi.raman@yahoo.com>
2023-04-17 19:13:50 +0430

    add NgForm and flexLayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { LoginComponent } from './auth/login/login.component';
@@ -28,7 +30,9 @@ import { WelcomeComponent } from './welcome/welcome.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-    imports: [MatFormFieldModule,MatInputModule],
-    exports: [MatFormFieldModule,MatInputModule]
+    imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+    exports: [MatFormFieldModule, MatInputModule, MatButtonModule]
 
 })
 export class MaterialModule {
